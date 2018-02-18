0870d75429fd990bac0bb3bf358c1f1a084faabe
gerhardboer <gboer@ilionx.com>
2018-02-18 15:18:15 +0100

    icon

---
+++ src/com/gerhardboer/fileditor/icon/NgComponentIconProvider.java
@@ -0,0 +1,25 @@
+package com.gerhardboer.fileditor.icon;
+
+import com.intellij.ide.IconProvider;
+import com.intellij.openapi.util.IconLoader;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+
+public class NgComponentIconProvider extends IconProvider {
+    @Nullable
+    @Override
+    public Icon getIcon(@NotNull PsiElement psiElement, int i) {
+        PsiFile containingFile = psiElement.getContainingFile();
+        if(containingFile != null) {
+            boolean isNgComponentView = containingFile.getVirtualFile().getExtension().equals("component-view");
+            if (isNgComponentView) {
+                return IconLoader.getIcon("/assets/ng-component-view.png");
+            }
+        }
+        return null;
+    }
+}
