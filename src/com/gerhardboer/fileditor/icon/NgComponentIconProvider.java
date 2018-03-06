3315691ff8ca635bd946f50e746cf8365b29439c
gerhardboer <gboer@ilionx.com>
2018-03-06 15:41:59 +0100

    nullpointer fix

--- src/com/gerhardboer/fileditor/icon/NgComponentIconProvider.java
+++ src/com/gerhardboer/fileditor/icon/NgComponentIconProvider.java
@@ -3,6 +3,7 @@ package com.gerhardboer.fileditor.icon;
 import com.gerhardboer.fileditor.Constants;
 import com.intellij.ide.IconProvider;
 import com.intellij.openapi.util.IconLoader;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import org.jetbrains.annotations.NotNull;
@@ -11,16 +12,22 @@ import org.jetbrains.annotations.Nullable;
 import javax.swing.*;
 
 public class NgComponentIconProvider extends IconProvider {
-    @Nullable
-    @Override
-    public Icon getIcon(@NotNull PsiElement psiElement, int i) {
-        PsiFile containingFile = psiElement.getContainingFile();
-        if (containingFile != null) {
-            boolean isNgComponentView = containingFile.getVirtualFile().getExtension().equals(Constants.EXTENSION);
-            if (isNgComponentView) {
-                return IconLoader.getIcon("/assets/ng-component-view.png");
-            }
-        }
-        return null;
+  @Nullable
+  @Override
+  public Icon getIcon(@NotNull PsiElement psiElement, int i) {
+    PsiFile containingFile = psiElement.getContainingFile();
+    if (containingFile == null) {
+      return null;
     }
+
+    VirtualFile file = containingFile.getVirtualFile();
+    if (file == null) {
+      return null;
+    }
+
+    boolean isNgComponentView = Constants.EXTENSION.equals(file.getExtension());
+    return isNgComponentView
+        ? IconLoader.getIcon("/assets/ng-component-view.png")
+        : null;
+  }
 }
